3201d299f9616ffd66246210a347348e69a0296f
Gordon Klundt <gklundt@gmail.com>
2017-10-15 13:10:42 -0500

    Rev 1.2 Small - Cleanup unused components

--- app/app.module.js
+++ app/app.module.js
@@ -1,21 +1,4 @@
 "use strict";
-// import {VioServicesComponent} from "./vio-services/vio-services.component";
-// import {VioServicesMdComponent} from "./vio-services-md/vio-services-md.component";
-// import {VioServicesSmComponent} from "./vio-services-sm/vio-services-sm.component";
-// import {VioBenefitsComponent} from "./vio-benefits/vio-benefits.component";
-// import {VioBenefitsMdComponent} from "./vio-benefits-md/vio-benefits-md.component";
-// import {VioBenefitsSmComponent} from "./vio-benefits-sm/vio-benefits-sm.component";
-// import {VioAboutComponent} from "./vio-about/vio-about.component";
-// import {VioAboutMdComponent} from "./vio-about-md/vio-about-md.component";
-// import {VioAboutSmComponent} from "./vio-about-sm/vio-about-sm.component";
-// import {VioHeaderMdComponent} from "./vio-header-md/vio-header-md.component";
-// import {VioHeaderSmComponent} from "./vio-header-sm/vio-header-sm.component";
-// import {VioPartnersComponent} from "./vio-partners/vio-partners.component";
-// import {VioPartnersMdComponent} from "./vio-partners-md/vio-partners-md.component";
-// import {VioPartnersSmComponent} from "./vio-partners-sm/vio-partners-sm.component";
-// import {VioContactsComponent} from "./vio-contacts/vio-contacts.component";
-// import {VioContactsMdComponent} from "./vio-contacts-md/vio-contacts-md.component";
-// import {VioContactsSmComponent} from "./vio-contacts-sm/vio-contacts-sm.component";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -29,7 +12,6 @@ var app_component_1 = require("./app.component");
 var ng2_bootstrap_1 = require("ng2-bootstrap");
 var angular2_fontawesome_1 = require("angular2-fontawesome/angular2-fontawesome");
 var vio_services_xs_component_1 = require("./vio-services-xs/vio-services-xs.component");
-var modal_service_1 = require("./modal.service");
 var vio_benefits_xs_component_1 = require("./vio-benefits-xs/vio-benefits-xs.component");
 var vio_about_xs_component_1 = require("./vio-about-xs/vio-about-xs.component");
 var vio_header_component_1 = require("./vio-header/vio-header.component");
@@ -42,13 +24,20 @@ var vio_home_component_1 = require("./vio-home/vio-home.component");
 var vio_nav_component_1 = require("./vio-nav/vio-nav.component");
 var vio_modal_request_xs_component_1 = require("./vio-modal-request-xs/vio-modal-request-xs.component");
 var vio_permit_xs_component_1 = require("./vio-permit-xs/vio-permit-xs.component");
+var modal_service_1 = require("./modal.service");
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule, ng2_bootstrap_1.AccordionModule.forRoot(), ng2_bootstrap_1.TabsModule.forRoot(), angular2_fontawesome_1.Angular2FontawesomeModule],
-            declarations: [app_component_1.AppComponent,
+            imports: [
+                platform_browser_1.BrowserModule,
+                ng2_bootstrap_1.AccordionModule.forRoot(),
+                ng2_bootstrap_1.TabsModule.forRoot(),
+                angular2_fontawesome_1.Angular2FontawesomeModule
+            ],
+            declarations: [
+                app_component_1.AppComponent,
                 vio_services_xs_component_1.VioServicesXsComponent,
                 vio_benefits_xs_component_1.VioBenefitsXsComponent,
                 vio_about_xs_component_1.VioAboutXsComponent,
@@ -62,23 +51,6 @@ var AppModule = (function () {
                 vio_permit_xs_component_1.VioPermitXsComponent,
                 vio_alert_xs_component_1.VioAlertXsComponent,
                 vio_home_component_1.VioHomeComponent
-                // , VioServicesComponent
-                // , VioServicesMdComponent
-                // , VioServicesSmComponent
-                // , VioBenefitsComponent
-                // , VioBenefitsMdComponent
-                // , VioBenefitsSmComponent
-                // , VioAboutComponent
-                // , VioAboutMdComponent
-                // , VioAboutSmComponent
-                // , VioHeaderMdComponent
-                // , VioHeaderSmComponent
-                // , VioPartnersComponent
-                // , VioPartnersMdComponent
-                // , VioPartnersSmComponent
-                // , VioContactsComponent
-                // , VioContactsMdComponent
-                // , VioContactsSmComponent
             ],
             providers: [modal_service_1.ModalService],
             bootstrap: [app_component_1.AppComponent]
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,21 +1,3 @@
-// import {VioServicesComponent} from "./vio-services/vio-services.component";
-// import {VioServicesMdComponent} from "./vio-services-md/vio-services-md.component";
-// import {VioServicesSmComponent} from "./vio-services-sm/vio-services-sm.component";
-// import {VioBenefitsComponent} from "./vio-benefits/vio-benefits.component";
-// import {VioBenefitsMdComponent} from "./vio-benefits-md/vio-benefits-md.component";
-// import {VioBenefitsSmComponent} from "./vio-benefits-sm/vio-benefits-sm.component";
-// import {VioAboutComponent} from "./vio-about/vio-about.component";
-// import {VioAboutMdComponent} from "./vio-about-md/vio-about-md.component";
-// import {VioAboutSmComponent} from "./vio-about-sm/vio-about-sm.component";
-// import {VioHeaderMdComponent} from "./vio-header-md/vio-header-md.component";
-// import {VioHeaderSmComponent} from "./vio-header-sm/vio-header-sm.component";
-// import {VioPartnersComponent} from "./vio-partners/vio-partners.component";
-// import {VioPartnersMdComponent} from "./vio-partners-md/vio-partners-md.component";
-// import {VioPartnersSmComponent} from "./vio-partners-sm/vio-partners-sm.component";
-// import {VioContactsComponent} from "./vio-contacts/vio-contacts.component";
-// import {VioContactsMdComponent} from "./vio-contacts-md/vio-contacts-md.component";
-// import {VioContactsSmComponent} from "./vio-contacts-sm/vio-contacts-sm.component";
-
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
@@ -23,8 +5,6 @@ import {AccordionModule, TabsModule} from "ng2-bootstrap";
 import {Angular2FontawesomeModule} from "angular2-fontawesome/angular2-fontawesome";
 
 import {VioServicesXsComponent} from "./vio-services-xs/vio-services-xs.component";
-import {ModalService} from "./modal.service";
-
 import {VioBenefitsXsComponent} from "./vio-benefits-xs/vio-benefits-xs.component";
 import {VioAboutXsComponent} from "./vio-about-xs/vio-about-xs.component";
 import {VioHeaderComponent} from "./vio-header/vio-header.component";
@@ -38,9 +18,17 @@ import {VioNavComponent} from "./vio-nav/vio-nav.component";
 import {VioModalRequestXsComponent} from "./vio-modal-request-xs/vio-modal-request-xs.component";
 import {VioPermitXsComponent} from "./vio-permit-xs/vio-permit-xs.component";
 
+import {ModalService} from "./modal.service";
+
 @NgModule({
-    imports: [BrowserModule, AccordionModule.forRoot(), TabsModule.forRoot(), Angular2FontawesomeModule],
-    declarations: [AppComponent
+    imports: [
+        BrowserModule
+        , AccordionModule.forRoot()
+        , TabsModule.forRoot()
+        , Angular2FontawesomeModule
+    ],
+    declarations: [
+        AppComponent
         , VioServicesXsComponent
         , VioBenefitsXsComponent
         , VioAboutXsComponent
@@ -54,23 +42,6 @@ import {VioPermitXsComponent} from "./vio-permit-xs/vio-permit-xs.component";
         , VioPermitXsComponent
         , VioAlertXsComponent
         , VioHomeComponent
-        // , VioServicesComponent
-        // , VioServicesMdComponent
-        // , VioServicesSmComponent
-        // , VioBenefitsComponent
-        // , VioBenefitsMdComponent
-        // , VioBenefitsSmComponent
-        // , VioAboutComponent
-        // , VioAboutMdComponent
-        // , VioAboutSmComponent
-        // , VioHeaderMdComponent
-        // , VioHeaderSmComponent
-        // , VioPartnersComponent
-        // , VioPartnersMdComponent
-        // , VioPartnersSmComponent
-        // , VioContactsComponent
-        // , VioContactsMdComponent
-        // , VioContactsSmComponent
     ],
     providers: [ModalService],
     bootstrap: [AppComponent]
